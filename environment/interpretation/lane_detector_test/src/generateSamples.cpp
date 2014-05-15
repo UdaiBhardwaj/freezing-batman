a53e760f437371c69fb570a73bbeb8f7ec23ed84
Udai <udai@Udai.(none)>
2014-05-15 16:37:08 +0530

    dynamic collection of machine learning data

--- environment/interpretation/lane_detector_test/src/generateSamples.cpp
+++ environment/interpretation/lane_detector_test/src/generateSamples.cpp
@@ -22,7 +22,7 @@ char WHITE[]= "\033[37;01m";
 
 std::ofstream commonFile;
 
-cv::Mat img,temp;
+cv::Mat img,temp, binary;
 
 std::string name;
 
@@ -69,15 +69,60 @@ void CallBackFunc(int event, int x, int y, int flags, void* userdata)
 }
 
 
+void CallBackFunc2(int event, int x, int y)
+{
+	int countP,countN;
+	//cv::Mat imgRoi;
+     if  ( event == 1){
+          std::cout << BLUE << "Saving positive sample - position (" << x << ", " << y << ")" << NORMAL << std::endl;
+         // imgRoi = temp(cv::Rect(x-size_sample/2, y-size_sample/2, size_sample, size_sample));
+          
+          commonFile<<"+1 ";
+          countP=0;
+          for(int i=y - size_sample/2;i< y+ size_sample/2; i++){
+			  for(int j= x - size_sample/2 ;j< x + size_sample/2 ;j++){
+				  commonFile<<countP++<<":"<<(int)img.at<cv::Vec3b>(i,j)[0]<<" ";
+				  commonFile<<countP++<<":"<<(int)img.at<cv::Vec3b>(i,j)[1]<<" ";
+				  commonFile<<countP++<<":"<<(int)img.at<cv::Vec3b>(i,j)[2]<<" ";
+			  }
+		  }
+          
+          commonFile<<countP++<<":-1"<<std::endl;
+          //cv::rectangle( img, cv::Point( x-size_sample/2, y-size_sample/2 ), cv::Point( x+size_sample/2, y+size_sample/2 ), cv::Scalar( 255, 255, 255 ));
+     }
+     else if  ( event == -1){
+		 std::cout << RED << "Saving negative sample - position (" << x << ", " << y << ")" << NORMAL << std::endl;
+		 //imgRoi = temp(cv::Rect(x-size_sample/2, y-size_sample/2, size_sample, size_sample));
+		 
+		 commonFile<<"-1 ";
+		 countN=0;
+          for(int i=y - size_sample/2;i< y+ size_sample/2; i++){
+			  for(int j= x - size_sample/2 ;j< x + size_sample/2 ;j++){
+				commonFile<<countN++<<":"<<(int)img.at<cv::Vec3b>(i,j)[0]<<" ";
+				commonFile<<countN++<<":"<<(int)img.at<cv::Vec3b>(i,j)[1]<<" ";
+				commonFile<<countN++<<":"<<(int)img.at<cv::Vec3b>(i,j)[2]<<" ";
+			  }
+		  }
+          
+          commonFile<<countN++<<":-1"<<std::endl;
+          //cv::rectangle( img, cv::Point( x-size_sample/2, y-size_sample/2 ), cv::Point( x+size_sample/2, y+size_sample/2 ), cv::Scalar( 0, 0, 0 ));
+     }
+}
+
+
+
+
+
 int main(int argc, char** argv) {
 	if(argc<2){
 		printf("Usage: <name> <image_file> <optional:= kernel_size>\n");
 		return -1;
 	}
-	commonFile.open("Samples",std::fstream::app);
+	commonFile.open("Samples2",std::fstream::app);
 	img=cv::imread(argv[1]);
+	binary = cv::imread("/home/udai/fuerte_workspace/sandbox/freezing-batman/environment/interpretation/lane_detector_test/lanebinary.jpg");
 	
-	if( argc == 3 ){
+	if(( argc == 3 )||(argc == 4)){
 		int temp_size_sample = atoi(argv[2]);
 		if( temp_size_sample>=5 && temp_size_sample<=30){
 			size_sample = temp_size_sample;
@@ -91,7 +136,32 @@ int main(int argc, char** argv) {
           return -2; 
     }
 	cv::namedWindow("Original Image");
-	cv::setMouseCallback("Original Image", CallBackFunc, NULL);
+	int choice = 0;
+	if(argc == 4) choice = atoi(argv[3]);
+	if(choice==1)
+	{	std::cout<<"Dynamic input\n";
+		int square_size = 80;
+		for(int i = img.rows/2 - square_size/2 + size_sample/2; i<(img.rows/2 + square_size/2 - size_sample/2); i+=size_sample)
+			for(int j = img.cols/2 - square_size/2 + size_sample/2; j<(img.cols/2 + square_size/2 - size_sample/2); j+=size_sample)
+			{
+				CallBackFunc2(1, j, i);
+			}
+		/*
+		for(int i = img.rows/2 - square_size/2; i< img.rows/2 + square_size/2; ++i)
+			for(int j = img.cols/2 - square_size/2; j<img.cols/2 + square_size/2; ++j)
+				CallBackFunc2(1, j, i);*/
+
+		for(int k = 0; k < binary.rows; ++k)
+			for(int l = 0; l<binary.cols; ++l)
+			{
+				int intensity = binary.at<uchar>(k, l);
+				if(intensity>240)
+					CallBackFunc2(-1, l, k);
+			}
+		std::cout<<"Exiting dynamic input\n";
+	}
+	else
+		cv::setMouseCallback("Original Image", CallBackFunc, NULL);
 	while(true){
 		cv::imshow("Original Image",img);
 		cv::waitKey(10);
